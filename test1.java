0d07b5b066b26f114fe7eaaf2d723b311d508e56
wangde-fu <zero.p@111.com>
2022-08-13 23:25:09 +0800

    t

--- test1.java
+++ test1.java
@@ -1,33 +1,58 @@
+import java.util.ArrayList;
 /*
 * 从一个装有n个球的箱子里取球，从中取出k个球时的得分为取出k个球时所有可能性的平方。
 * 求对于所有可能的k，得分值之和。输出该得分对1e9+7的模。
 * 例：假设共有3个球，得分为3*3+3*3+1=19。
 */
 class test1{
-    //public static  int f(int n,int m){//在n个球中取m个球
-    //    if (m==0||m==n){return 1;}
-    //    return f(n-1,m-1)+f(n-1,m);
-    //}
+    static int qmi(int a,int k,int p) {
+        //快速幂预处理 a^k%p  将k拆成二进制
+        long res=1;
+        while(k>0) {
+            if((k&1)!=0) {
+                res = res * a % p;
+            }
+            a=(int)((long)a*a%p);
+            k>>=1;
+        }
+        return (int)res;
+    }
+
+    public static int fact(int j,int p){
+        int tmp=1;
+        for (int i = 1; i < (j+1); i++) {
+            tmp=(int)((long)tmp*i%p);
+        }
+        return tmp;
+    }
+    public static int infact(int j,int p){
+        int tmp=1;
+        for (int i = 1; i < (j+1); i++) {
+            //费马小定理求i的逆元阶乘
+            tmp=(int)((long)tmp*qmi(i,p-2,p)%p);
+        }
+        return tmp;
+    }
 
     public static void main(String[] args){
-        int n=50;
-        long la[][]=new long[n+1][n+1];
-        for (int i = 0; i < n+1; i++) {
-            for (int j = 0; j < i+1; j++) {
-                if (j==0||j==i){
-                    la[i][j]=1;
-                }
-                else{
-                    la[i][j]=la[i-1][j-1]+la[i-1][j];
-                }
-            }
-        }
+        //https://blog.csdn.net/weixin_45480785/article/details/113896599,快速幂求逆元，卢卡斯定理
+        int n=50;int p=(int)1e9+7;
+        int res=(int)((long)fact(2*n,p)*infact(n,p)%p*infact(n,p)%p);
+        System.out.println(res-1);
 
+        /*ArrayList<Long> tmp=new ArrayList<>();
+        tmp.add((long)1);
+        for (int i = 0; i <n; i++) {
+            for (int j = i; j >0 ; j--) {
+                tmp.set(j, tmp.get(j)+ tmp.get(j-1));
+            }
+            tmp.add((long)1);
+        }
         long sum=0L;
-        for (long m:la[n]) {
-            sum+=(m*m);
+        for (long m:tmp){
+            sum+=m*m;
         }
         sum-=1;
-        System.out.println(sum%(1e9+7));
+        System.out.println(sum%(1e9+7));*/
     }
 }
